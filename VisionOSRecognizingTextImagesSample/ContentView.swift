951059e759055263e30cd48a7ab1438348ba522f
tokufxug <tokufxug@gmail.com>
2024-03-29 22:23:32 +0900

    Initial Commit

---
+++ VisionOSRecognizingTextImagesSample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionOSRecognizingTextImagesSample
+//
+//  Created by Sadao Tokuyama on 3/29/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionOSRecognizingTextImagesSample/VisionOSRecognizingTextImagesSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOSRecognizingTextImagesSampleApp.swift
+//  VisionOSRecognizingTextImagesSample
+//
+//  Created by Sadao Tokuyama on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSRecognizingTextImagesSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
